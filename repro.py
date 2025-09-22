55936bef387d412ed68cfe877b9ecf6a929bd661
Alex N. Jose <alexnj@google.com>
2025-09-22 10:25:06 -0700

    provide user-data-dir explicitly

--- repro.py
+++ repro.py
@@ -17,12 +17,17 @@ class ReproTest(unittest.TestCase):
         chrome_opts = ChromeOptions()
         chrome_binary = os.getenv('CHROMIUM_BIN')
         chromedriver_binary = os.getenv('CHROMEDRIVER_BIN')
+
+        # Create a temp directory for user-data-dir
+        temp_dir = self.CreateTempDir()
+
         if chrome_binary:
             # Set Chrome options suitable for the continuous build
             chrome_opts.binary_location = chrome_binary
             chrome_opts.add_argument('--headless')
             chrome_opts.add_argument('--disable-gpu')
             chrome_opts.add_argument('--verbose')
+            chrome_opts.add_argument('--user-data-dir=%s' % temp_dir)
         cls.selenium = WebDriver(options=chrome_opts, service=Service(executable_path=chromedriver_binary, service_args=['--verbose']))
         cls.selenium.implicitly_wait(9)
 
