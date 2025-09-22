313f94235b7d6b01be16478fbe2850be6401ba0f
Alex N. Jose <alexnj@google.com>
2025-09-22 11:35:38 -0700

    Apply no sandbox

--- repro.py
+++ repro.py
@@ -25,13 +25,14 @@ class ReproTest(unittest.TestCase):
         print('--user-data-dir=%s' % temp_dir)
         print(chrome_binary)
         print(chromedriver_binary)
-        
+
         if chrome_binary:
             # Set Chrome options suitable for the continuous build
             chrome_opts.binary_location = chrome_binary
             chrome_opts.add_argument('--headless=new')
             chrome_opts.add_argument('--disable-gpu')
             chrome_opts.add_argument('--verbose')
+            chrome_opts.add_argument('--no-sandbox')
             # chrome_opts.add_argument('--user-data-dir=%s' % temp_dir)
         cls.selenium = WebDriver(options=chrome_opts, service=Service(executable_path=chromedriver_binary, service_args=['--verbose']))
         cls.selenium.implicitly_wait(9)
