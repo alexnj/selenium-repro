4b0ad45265a730b8ce53f3ccc05a25a92d1f4590
Alex N. Jose <alexnj@google.com>
2025-09-22 10:54:48 -0700

    omit user-data-dir / headless-new

--- repro.py
+++ repro.py
@@ -23,14 +23,16 @@ class ReproTest(unittest.TestCase):
         # Create a temp directory for user-data-dir
         temp_dir = "/home/runner/work/selenium-repro/selenium-repro/assigned-temp"
         print('--user-data-dir=%s' % temp_dir)
-
+        print(chrome_binary)
+        print(chromedriver_binary)
+        
         if chrome_binary:
             # Set Chrome options suitable for the continuous build
             chrome_opts.binary_location = chrome_binary
-            chrome_opts.add_argument('--headless')
+            chrome_opts.add_argument('--headless=new')
             chrome_opts.add_argument('--disable-gpu')
             chrome_opts.add_argument('--verbose')
-            chrome_opts.add_argument('--user-data-dir=%s' % temp_dir)
+            # chrome_opts.add_argument('--user-data-dir=%s' % temp_dir)
         cls.selenium = WebDriver(options=chrome_opts, service=Service(executable_path=chromedriver_binary, service_args=['--verbose']))
         cls.selenium.implicitly_wait(9)
 
