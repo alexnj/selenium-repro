ed1847da536c401de70e12922a83605980e25cd5
Alex N. Jose <alexnj@google.com>
2025-09-24 09:00:18 -0700

    Missed lines

--- repro.py
+++ repro.py
@@ -24,9 +24,10 @@ class ReproTest(unittest.TestCase):
             chrome_opts.add_argument('--disable-gpu')
             chrome_opts.add_argument('--verbose')
             chrome_opts.add_argument('--remote-debugging-pipe')
-        cls.selenium = WebDriver(options=chrome_opts,
-                service = Service(executable_path=chromedriver_binary,
-                service_args=['--verbose'], log_output='chromedriver.log'))
+        service = Service(executable_path=chromedriver_binary,
+                service_args=['--verbose'], log_output='chromedriver.log')
+        cls.selenium = WebDriver(options=chrome_opts, service=service)
+        cls.selenium.implicitly_wait(10)
 
     @classmethod
     def tearDownClass(cls):
